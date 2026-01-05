1add0c2d90b8079282f879dd5a451a7158047e0a
Afroartus <andresro445@gmail.com>
2026-01-05 15:37:33 -0600

    chore: initial project setup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	run()
+}
+
+func run() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
