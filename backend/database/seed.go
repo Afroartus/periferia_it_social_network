fbbb8dd83969838c48d3adc1c20cbaa54e17e34e
Afroartus <andresro445@gmail.com>
2026-01-06 00:02:57 -0600

    refact: reorganize project structure separating backend and frontend

--- backend/database/seed.go
+++ backend/database/seed.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"periferia_it_social_network/models"
+	models2 "periferia_it_social_network/models"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -9,14 +9,14 @@ import (
 
 func Seed() {
 	var count int64
-	DB.Model(&models.User{}).Count(&count)
+	DB.Model(&models2.User{}).Count(&count)
 	if count > 0 {
 		return
 	}
 
 	password, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
 
-	user := models.User{
+	user := models2.User{
 		Name:      "John",
 		LastName:  "Perez",
 		Username:  "JohnP",
@@ -27,7 +27,7 @@ func Seed() {
 
 	DB.Create(&user)
 
-	post := models.Post{
+	post := models2.Post{
 		Message: "Create a new post",
 		UserID:  user.ID,
 	}
--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"periferia_it_social_network/database"
-	"periferia_it_social_network/models"
+	database2 "periferia_it_social_network/database"
+	models2 "periferia_it_social_network/models"
 	"periferia_it_social_network/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	database.ConnectDB()
+	database2.ConnectDB()
 
-	database.DB.AutoMigrate(
-		&models.User{},
-		&models.Post{},
+	database2.DB.AutoMigrate(
+		&models2.User{},
+		&models2.Post{},
 	)
-	database.Seed()
+	database2.Seed()
 	run()
 }
 
